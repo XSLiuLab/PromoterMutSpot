341741d37a0953c3423b29f77b2b89574a174ce4
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-05-17 00:10:25 +0800

    Update main2.R

--- main2.R
+++ main2.R
@@ -1,5 +1,12 @@
 load("data/mutationList.RData")
 load("data/all_patients.RData")
+load("data/all_positions.RData")
+
+icgc_ids <- data.table::fread("data/icgc_projects_and_ids.tsv")
+icgc_ids <- icgc_ids[icgc_donor_id %in% all_patients]
+
+## Should use all ICGC ids? or all_patienst? or all ids with mutation left in promoters?
+## use the second appraoch for now.
 
 type_list = list(
   lung = c("LUSC-CN", "LUSC-KR"),
@@ -12,6 +19,13 @@ type_list = list(
   ovary = c("OV-AU"),
   melanoma = c("MELA-AU","SKCA-BR","SKCM-US")
 )
+
+## For each cancer type,
+## generate the data.frame used for training and fitting
+
+patient_dt = mut_df[, .(chr, position, pos)][rep(seq(nrow(mut_df)), each = nrow(icgc_ids)),]
+patient_dt[, patient := rep(icgc_ids$icgc_donor_id, nrow(mut_df))]
+
 
 ## Step5: construct patient-specific background mutation probability model (based on logistic)
 
